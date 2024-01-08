bc05b8e7db34dfc9ae730726002ae76dcd0ca502
Eric <milkhoney911777@gmail.com>
2024-01-08 14:37:09 +0800

    New Project initrialize

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace THM104_01
+{
+    internal class Program
+    {
+
+            static void Main(string[] args)
+            {
+                var connectionString = "Data Source=.;Initial Catalog=northwind;Integrated Security=True;Trust Server Certificate=True"
+                SqlConnection cn = new SqlConnection(connectionString);
+                cn.Open();
+                string qurey = "select * from customers";
+                SqlCommand cmd = new SqlCommand(qurey, cn);
+
+                SqlDataReader reader = cmd.ExecuteReader();//指到標題
+                while (reader.Read())//Read()嘗試指到第一行，指到第一行就會回true，判斷有沒有下一筆
+                {
+                    Console.WriteLine(reader["city"].ToString());
+                }
+
+
+                Console.WriteLine("Hello, World!");
+            }
+        
+    }
+}
+
